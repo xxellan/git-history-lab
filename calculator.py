67752c5cb4e63875034fafcd0984adfc2237bbf1
chetverikova.valeriya <chetverikova.valerka@gmail.com>
2025-12-26 23:15:28 +0400

    Добавлены функции умножения и вычитания

--- calculator.py
+++ calculator.py
@@ -4,3 +4,7 @@ def divide(a, b):
     if b == 0: 
         raise ValueError("Деление на ноль невозможно") 
     return a / b 
+def multiply(a, b): 
+    return a * b 
+def subtract(a, b): 
+    return a - b 
